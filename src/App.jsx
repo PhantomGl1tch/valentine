5b166cf177839933bf518a4126649f02e3c15735
Juan Jose Lozano <126698860+PhantomGl1tch@users.noreply.github.com>
2026-02-06 01:37:09 +0100

    innit commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,24 +14,24 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Plsss? :( You're breaking my heart",
+      "Seguro?",
+      "Seguro seguro?",
+      "Seguro segurisimo que no?",
+      "Porfi Omar :(",
+      "Ultima oportunidad, seguro que no?",
+      "Ultima a la de una...",
+      "Ultima a la de dos...",
+      "A la de dos y media...",
+      "Dos y tres cuartos...",
+      "Pero yo te quiero mucho :c",
+      "Porfiiii te hare tarta",
+      "Porfiiii wiwiwi",
+      "Puedo hacerte cambiar de opinon?",
+      "Porfiiii wiwiwi",
+      "Puedo hacerte cambiar de opinon?",
+      "Si me dices que si dejo de jugar Slayer",
+      "Porfa pookieeeeee",
+      "Porfi que yo te quiero mucho darling",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -61,7 +61,7 @@ export default function Page() {
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.webp"
           />
           <h1 className="text-4xl md:text-6xl my-4 text-center">
-            Will you be my Valentine?
+            ¿Quieres ser mi San Valentin?
           </h1>
           <div className="flex flex-wrap justify-center gap-2 items-center">
             <button
@@ -69,7 +69,7 @@ export default function Page() {
               style={{ fontSize: yesButtonSize }}
               onClick={() => setYesPressed(true)}
             >
-              Yes
+              SI
             </button>
             <button
               onClick={handleNoClick}
@@ -84,18 +84,3 @@ export default function Page() {
     </div>
   );
 }
-
-const Footer = () => {
-  return (
-    <a
-      className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
-      href="https://github.com/Xeven777/valentine"
-      target="__blank"
-    >
-      Made with{" "}
-      <span role="img" aria-label="heart">
-        ❤️
-      </span>
-    </a>
-  );
-};
